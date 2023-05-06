dcbefbdc90fe0b7c7325609632750a9c8d5a518d
AdamAzuddin <05adamaz@gmail.com>
2023-05-06 14:35:49 +0800

    Done with level 1 security

--- app.js
+++ app.js
@@ -1,28 +1,74 @@
 //jshint esversion:6
 
-const express = require("express")
-const bodyParser = require("body-parser")
-const ejs = require("ejs")
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-const app = express()
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended:true}))
-app.set("view engine", "ejs")
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String,
+});
 
-app.get("/", function (req,res){
-    res.render("home")
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.route("/login")
+.get((req,res)=>{
+  res.render("login")
+})
+.post(async (req,res)=>{
+  const username = req.body.username
+  const password = req.body.password
+
+  const findUser = await User.findOne({email: username}).exec()
+  if (findUser){
+    if (findUser.password === password){
+      res.render("secrets")
+    } else{
+      res.send("Wrong username or password")
+    }
+  } else{
+    res.send("User not found")
+  }
 })
 
-app.get("/login", function (req,res){
-    res.render("login")
-})
+app
+  .route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post((req, res) => {
+    const newEmail = req.body.username;
+    const newPassword = req.body.password;
 
-app.get("/register", function (req,res){
-    res.render("register")
-})
-app.listen(3000, function(){
-    console.log("Server started at port 3000");
-})
+    console.log(newEmail);
 
+    console.log(newPassword);
+
+    const newUser = User({
+      email: newEmail,
+      password: newPassword,
+    });
+
+    newUser.save().then(function (successful) {
+        if (successful) {
+          res.render("secrets")
+        } else {
+          console.log("An error occured");
+        }
+      });
+  });
+app.listen(3000, function () {
+  console.log("Server started at port 3000");
+});
