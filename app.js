a82dc0d9de4ce7a56f1214ccff3e85471bffd9ea
AdamAzuddin <05adamaz@gmail.com>
2023-05-06 16:02:50 +0800

    Add bcrypt

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5")
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 app.use(express.static("public"));
@@ -13,7 +14,7 @@ app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
-const userSchema =new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
@@ -24,25 +25,29 @@ app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.route("/login")
-.get((req,res)=>{
-  res.render("login")
-})
-.post(async (req,res)=>{
-  const username = req.body.username
-  const password = md5(req.body.password)
+app
+  .route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post(async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-  const findUser = await User.findOne({email: username}).exec()
-  if (findUser){
-    if (findUser.password === password){
-      res.render("secrets")
-    } else{
-      res.send("Wrong username or password")
+    const findUser = await User.findOne({ email: username }).exec();
+    if (findUser) {
+      bcrypt.compare(password, findUser.password, function (err, passwordMatch) {
+        // result == true
+        if (passwordMatch){
+          res.render("secrets")
+        } else{
+          res.send("Wrong password")
+        }
+      });
+    } else {
+      res.send("User not found");
     }
-  } else{
-    res.send("User not found")
-  }
-})
+  });
 
 app
   .route("/register")
@@ -51,20 +56,22 @@ app
   })
   .post((req, res) => {
     const newEmail = req.body.username;
-    const newPassword = md5(req.body.password)
+    const newPassword = req.body.password;
+    bcrypt.hash(newPassword, saltRounds, function (err, hash) {
+      // Store hash in your password DB.
+      const newUser = User({
+        email: newEmail,
+        password: hash,
+      });
 
-    const newUser = User({
-      email: newEmail,
-      password: newPassword,
-    });
-
-    newUser.save().then(function (successful) {
+      newUser.save().then(function (successful) {
         if (successful) {
-          res.render("secrets")
+          res.render("secrets");
         } else {
           console.log("An error occured");
         }
       });
+    });
   });
 app.listen(3000, function () {
   console.log("Server started at port 3000");
